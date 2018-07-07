3b6e2f26bd959a31ff2b9990caf9d607dfbcd3dd
BuildTools <unconfigured@null.spigotmc.org>
2018-07-06 17:38:58 -0700

    fit 4 images per row

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const YOUR_API_KEY = "3ROM94mJAtgC2aPffCeTWNZSDDTSaZ0i";
 window.onload = function() {
     let offset = 0;
-    let xhr = $.get(`http://api.giphy.com/v1/gifs/trending?api_key=${YOUR_API_KEY}&limit=20&offset=${offset}`);
-    xhr.done(function(data) { console.log("success got data", data); });
+    const content = document.querySelector(".content");
+    const xhr = $.get(`http://api.giphy.com/v1/gifs/trending?api_key=${YOUR_API_KEY}&limit=20&offset=${offset}`);
 
-    $("img").hover(function(){
+    xhr.done(function(data) { 
+        let result = [];
+        console.log("success got data", data); 
+        const gifs = data['data'];
+        gifs.forEach(element => {
+            const url = element['images']['original_still']['url'];
+            result.push(`<img src=${url} style="max-width:248px; height:auto;">`)
+        });
+        content.innerHTML = result.join("");
+    
+        /* switch between static image and gif on hover */
+    $(".content img").hover(function(){
         const src = $(this).attr("src");
         $(this).attr("src",src.replace("giphy_s","giphy"));
-    },
-        function() {
-            const src = $(this).attr("src");
-            const length = src.length;
-            const newSrc = src.substring(0, length-9) + "giphy_s.gif";
-            $(this).attr("src",newSrc);
-        })
+     },
+    function() {
+        const src = $(this).attr("src");
+        const length = src.length;
+        const newSrc = src.substring(0, length-9) + "giphy_s.gif";
+        $(this).attr("src",newSrc);
+})
+    });
+
+
 }
 
