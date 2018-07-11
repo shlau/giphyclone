27ab2d64208981ae4e5f881695b78f1357ebc298
BuildTools <unconfigured@null.spigotmc.org>
2018-07-10 21:47:13 -0700

    added search functionality

--- main.js
+++ main.js
@@ -1,47 +1,74 @@
 const YOUR_API_KEY = "3ROM94mJAtgC2aPffCeTWNZSDDTSaZ0i";
 window.onload = function() {
+    let searchClicked = false;
     let offset = 0;
+    let searchVal;
     const content = document.querySelector(".content");
-    function loadImages() {
-        const xhr = $.get(`http://api.giphy.com/v1/gifs/trending?api_key=${YOUR_API_KEY}&limit=20&offset=${offset}`);
-        xhr.done(function(data) { 
-            let result = [];
-            console.log("success got data", data); 
-            const gifs = data['data'];
-            result.push(`<div class="item">`)
-            gifs.forEach(element => {
-                const url = element['images']['original_still']['url'];
+    const button = this.document.querySelector(".search img");
+ 
+    function displayImages(data) { 
+        let result = [];
+        console.log("success got data", data); 
+        const gifs = data['data'];
+        result.push(`<div class="item">`)
+        gifs.forEach(element => {
+            const url = element['images']['original_still']['url'];
 
-                result.push(`<img src=${url} style="width:248px; height:auto;">`)
+            result.push(`<img src=${url} style="width:248px; height:auto;">`);
 
-            });
-            result.push(`</div>`)
-            content.innerHTML += result.join("");
-        
-
-            /* switch between static image and gif on hover */
-            $(".content img").hover(function(){
-                const src = $(this).attr("src");
-                $(this).attr("src",src.replace("giphy_s","giphy"));
-            },
-            function() {
-                const src = $(this).attr("src");
-                const length = src.length;
-                const newSrc = src.substring(0, length-9) + "giphy_s.gif";
-                $(this).attr("src",newSrc);
-            })
         });
+        result.push(`</div>`);
+        const firstLoad = (offset == 0);
+        firstLoad ? content.innerHTML = result.join("") : content.innerHTML += result.join("");
+    
+
+        /* switch between static image and gif on hover */
+        $(".content img").hover(function(){
+            const src = $(this).attr("src");
+            $(this).attr("src",src.replace("giphy_s","giphy"));
+        },
+        function() {
+            const src = $(this).attr("src");
+            const length = src.length;
+            const newSrc = src.substring(0, length-9) + "giphy_s.gif";
+            $(this).attr("src",newSrc);
+        })
+    }
+
+    function loadImages() {
+        const xhr = $.get(`http://api.giphy.com/v1/gifs/trending?api_key=${YOUR_API_KEY}&limit=40&offset=${offset}`);
+        xhr.done(displayImages);
     }
 
     loadImages();
+    
     /* load images when end of page is reached */
     window.onscroll = function(ev) {
         if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
+          endReached = true;
           // you're at the bottom of the page
           offset += 40;
           console.log("Bottom of page ", offset );
-          loadImages();
+          searchClicked ? loadSearch() : loadImages();
         }
       };
+
+      function loadSearch() {
+          const formattedSearch = searchVal.replace(" ", "+");
+          console.log("search value", formattedSearch);
+          const xhr = $.get(`http://api.giphy.com/v1/gifs/search?q=${formattedSearch}&api_key=${YOUR_API_KEY}&limit=40&offset=${offset}`);
+          xhr.done(displayImages);
+
+      }
+
+      function handleSearch() {
+          searchVal = document.querySelector("input").value;
+          searchClicked = true;
+          offset = 0;
+          const headerText = document.querySelector(".trending h3");
+          headerText.innerHTML = searchVal.toUpperCase();
+          loadSearch();
+      }
+      button.addEventListener('click', handleSearch);
 }
 
